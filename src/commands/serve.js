f3c375c204d8d5966a572b0a12f51f663ec22088
Caio Tarifa <caiotarifa@gmail.com>
2020-09-04 17:10:08 -0300

    removing comments

--- src/commands/serve.js
+++ src/commands/serve.js
@@ -68,57 +68,5 @@ module.exports = {
     } catch (error) {
       serverSpinner.fail('Error launching application.')
     }
-
-
-    // const routesSpinner = print.spin('Registering routes...')
-    // const { routes } = require('../utils/routes.js')
-
-    // const server = express()
-    // server.use(bodyParser.json())
-
-    // for (const route of routes) {
-    //   server[route.method](route.path, (request, response, next) =>
-    //     route.action(request, response).then(next).catch(next)
-    //   )
-    // }
-
-    // routesSpinner.succeed('Successfully registered routes.')
-
-    // const serverSpinner = print.spin('Launching application...')
-
-    // const settings = getSettings(toolbox)
-    // const port = options.port || options.p || settings.server.port
-
-    // // actions
-    // const CreateActions = require('../utils/actions/createActions')
-    // const { loadModels, models, getEntityByModelName } = require('../utils/models')
-
-    // const createConnection = require('../utils/connection')
-    
-    // loadModels(toolbox)
-    // const actions = new CreateActions(server, models)
-
-    // createConnection(toolbox).then(() => {
-    //   Object.keys(models).forEach(model => {
-    //     actions.fetchById(getEntityByModelName(model))
-    //     actions.fetchList(getEntityByModelName(model))
-    //     actions.create(getEntityByModelName(model))
-    //     actions.update(getEntityByModelName(model))
-    //     actions.destroy(getEntityByModelName(model))
-    //   })
-    // })
-
-    // try {
-    //   server.listen(port, () => {
-    //     serverSpinner.succeed('Application has been launched.')
-    //     const url = `http://localhost:${port}`
-
-    //     breakLine()
-    //     print.info(`🚀 ${url}`)
-    //     breakLine()
-    //   })
-    // } catch (error) {
-    //   serverSpinner.fail('Error launching application.')
-    // }
   }
 }
--- src/commands/sync.js
+++ src/commands/sync.js
@@ -3,51 +3,5 @@ module.exports = {
 
   run: async toolbox => {
     require('../utils/connection')(toolbox)
-    // const {
-    //   parameters: { options },
-    //   print
-    // } = toolbox
-
-    // const {
-    //   loadModels,
-    //   getEntityByModelName,
-    //   models
-    // } = require('../utils/models')
-
-    // const modelsSpinner = print.spin('Loading models...')
-
-    // try {
-    //   loadModels(toolbox)
-    //   modelsSpinner.succeed('Models loaded.')
-    // } catch (error) {
-    //   modelsSpinner.fail('Error loading models.')
-    // }
-
-    // const typeorm = require('typeorm')
-    // const EntitySchema = typeorm.EntitySchema
-
-    // const entities = Object.keys(models).map(
-    //   model => new EntitySchema(getEntityByModelName(model))
-    // )
-
-    // const { getSettings } = require('../utils/settings')
-    // const settings = getSettings(toolbox)
-
-    // const syncSpinner = print.spin('Synchronizing...')
-
-    // try {
-    //   await typeorm.createConnection({
-    //     ...settings.database,
-
-    //     dropSchema: options.reset || options.r,
-    //     synchronize: true,
-  
-    //     entities
-    //   })
-
-    //   syncSpinner.succeed('Synchronized.')
-    // } catch (error) {
-    //   syncSpinner.fail('Synchronization error.')
-    // }
   }
 }
