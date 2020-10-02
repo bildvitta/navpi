980583c801e9a7116a166632610d298815146fa1
Douglas Calora <douglascalora123@gmail.com>
2020-10-02 13:50:09 -0300

    changes for PR

--- src/commands/seed.js
+++ src/commands/seed.js
@@ -15,13 +15,13 @@ module.exports = {
     const { connection, models } = await getConnection(toolbox)
 
     function getValue (model) {
-      model = models[model].fields
+      fields = models[model].fields
 
       const value = {}
 
-      for (const key in model) {
+      for (const key in fields) {
         value[key] = seeder(
-          seeder(model[key].__value || seederTypes[model[key].type])
+          seeder(fields[key].__value || seederTypes[fields[key].type])
         )
       }
 
